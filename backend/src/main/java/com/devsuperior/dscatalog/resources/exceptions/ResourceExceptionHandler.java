71edc93ed69315e237819c8c78217c1ff429c7fc
Leonardo A. de Souza <leo26gpl@gmail.com>
2021-01-14 15:37:28 -0300

    Find Category by id, exception handling

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+//Anotação que permite que essa classe intercepte exceções no controller
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+//Diz que tipo de exceção que esse método irá interceptar
+//Sempre que ocorrer uma exceção desse tipo em um dos controladores Rest, o tratamento da exceção será direcionado
+//para este método
+    @ExceptionHandler(EntityNotFoundException.class)
+    //HttpServletRequest tem as informações da requisição
+    public ResponseEntity<StandardError> entityNotFound (EntityNotFoundException e, HttpServletRequest request) {
+        StandardError error = new StandardError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setError("Resource not found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
