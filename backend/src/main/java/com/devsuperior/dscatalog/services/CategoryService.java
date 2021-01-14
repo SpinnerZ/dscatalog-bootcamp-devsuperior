ca90bd4cd7297f882a208bb4aa10dd54c8375ec8
Leonardo A. de Souza <leo26gpl@gmail.com>
2021-01-14 14:08:21 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,10 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    //Controla as transações do banco de dados, abrindo e fechando as conexões automaticamente.
+    // Torna a aplicação mais simples, fácil e eficiente.
+    //O readOnly é para ganho de performance e evita perda de informação em caso de erro, pois não marca o bd para escrita.
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
