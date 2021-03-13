486a26f31aca07b5b60ebb45febadd7d16b4c6a2
Leonardo A. de Souza <leo26gpl@gmail.com>
2021-03-13 16:36:11 -0300

    Import from DevSuperior to sync for module 2

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,79 +1,97 @@
 package com.devsuperior.dscatalog.entities;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant createdAt;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant updatedAt;
+	public Category() {
+	}
 
-    public Category() {
-    }
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
 
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
 
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    @PrePersist
-    public void prePersist() {
-        createdAt = Instant.now();
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        updatedAt = Instant.now();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Category category = (Category) o;
-
-        return id.equals(category.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id.hashCode();
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscatalog.services.exceptions;
 
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
 }
